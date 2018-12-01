2e13290280caa6cf7ce491de6a7a9cf0439c20d3
Paoge Moua <paoge.moua@gmail.com>
2018-12-01 00:20:16 -0600

    New updates

--- liri.js
+++ liri.js
@@ -1,3 +1,46 @@
+require("dotenv").config();
+
+var spotify = new Spotify(keys.spotify);
+
+// concert-this :: This will search the Bands in Town Artist Events API for an artist and render the following information about each event to the terminal:
+// name of the venue, venue location, date of the event (use moment to format this as "MM/DD/YYYY")
+var axios = require("axios");
+
+var nodeArgs = process.argv;
+
+var movieName = "";
+
+for (var i = 2; i < nodeArgs.length; i++) {
+
+    if (i > 2 && i < nodeArgs.length) {
+        movieName = movieName + "+" + nodeArgs[i];
+    }
+    else {
+        movieName += nodeArgs[i];
+    }
+}
+
+var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+console.log(queryUrl);
+
+axios.get(queryUrl).then(
+    function(response) {
+        console.log(response);
+    }
+);
+
+// spotify-this-song :: This will show the following information about the song in your terminal/bash window:
+// artist(s), the song's name, a preview link of the song from Spotify, the album that the song is from
+
+// movie-this :: This will output the following information to your terminal/bash window:
+// title, year, IMDB rating, rotten tomatoes rating, country, language, plot, actors
+// default :: 'Mr. Nobody'
+
+// do-what-it-says
+// LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands
+// run spotify-this-song for "I Want It That Way" as follows the text in random.txt; edit the text in random.txt to test out the feature for movie-this and concert-this
+
 // Include all NPM packages
 var spotify = require("node-spotify-api");
 var axios = require("axios");
